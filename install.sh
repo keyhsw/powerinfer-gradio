cb541a7f81ccecf72bd16402a6c5ea6bb051eeb4
key <30455795+keyhsw@users.noreply.github.com>
2024-01-03 13:38:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@ export OPENXLAB_AK="gnoewxwn5dznke163mxz"
 export OPENXLAB_SK="qk5ok2gvwpyabxe7qw60qgogk36rlagojernmwdj"
 openxlab model download --model-repo 'yixinsong/Falcon-40B' --model-name 'Falcon-40B'
 CMAKE_ARGS="-DLLAMA_CUBLAS=on" python3 -m pip install git+https://github.com/hodlen/llama-cpp-python.git@perf --force-reinstall
+pip install openpyxl
+pip uninstall gradio
+pip install gradio==4.12.0
